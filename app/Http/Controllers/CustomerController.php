d88392a43fd780ee704750e465eaf89a91dc3bb3
abhishek <abhishek.b@bindapi.com>
2024-06-10 11:36:27 +0530

    first commit

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Customer;
+
+class CustomerController extends Controller
+{
+    public function create()
+    {
+        $url = url('/customer');
+        $title = "Customer  Registration";
+        $data = compact('url');
+        return view('customer')->with($data);
+    }
+
+    public function store(Request $request)
+    {
+        // p($request->all());
+        // die;
+
+        /////// insert query
+        $customer = new Customer;
+        $customer->name = $request['name'];
+        $customer->email = $request['email'];
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->state = $request['state'];
+        $customer->country = $request['country'];
+        $customer->dob = $request['dob'];
+        $customer->password = md5($request['password']);
+        $customer->save();
+
+        return redirect('/customer/view');
+    }
+
+    public function view()
+    {
+        $title = "Customer List";
+        $url = url('/customer');
+        $customer = Customer::all();
+        $data = compact('customer', 'url', 'title');
+        return view('customer-view')->with($data);
+    }
+
+    public function delete($id)
+    {
+        Customer::find($id)->delete();
+        return redirect()->back();
+    }
+
+    public function edit($id)
+    {
+        $customer = Customer::find($id);
+        if (is_null($customer)) {
+            return redirect('customer');
+        }
+        $title = "Update Customer"; // Fixed the typo here
+        $url = url('/customer/update') . "/" . $id;
+        $data = compact('customer', 'url', 'title');
+
+        return view('customer')->with($data);
+    }
+
+    public function update($id, Request $request)
+    {
+        $customer = Customer::find($id);
+        $customer->name = $request['name'];
+        $customer->email = $request['email'];
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->state = $request['state'];
+        $customer->country = $request['country'];
+        $customer->dob = $request['dob'];
+        $customer->save();
+        return redirect('customer/view');
+
+    }
+}
