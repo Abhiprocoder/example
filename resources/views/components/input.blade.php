1fecde9ce9a5e549acdf7979645398f906aff4b8
abhishek <abhishek.b@bindapi.com>
2024-06-10 11:36:27 +0530

    first commit

---
+++ resources/views/components/input.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <!-- Nothing in life is to be feared, it is only to be understood. Now is the time to understand more, so that we may fear less. - Marie Curie -->
+</div>
+<div class="form-group">
+    <label for="">{{$label}}</label>
+    <input type="{{$type}}" name="{{$name}}" id="" class="form-control border-danger">
+    <span class="text-danger">
+        
+    </span>
+</div>
